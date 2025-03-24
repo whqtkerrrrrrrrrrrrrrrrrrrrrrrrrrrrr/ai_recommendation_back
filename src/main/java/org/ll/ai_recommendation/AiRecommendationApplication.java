093c9f169d42f9f289210295209ba78b7bc68a4b
seonghyeok <chosh2001@naver.com>
2025-03-24 19:38:17 +0900

    fix: EnableJpaAuditing 추가

--- src/main/java/org/ll/ai_recommendation/AiRecommendationApplication.java
+++ src/main/java/org/ll/ai_recommendation/AiRecommendationApplication.java
@@ -2,8 +2,10 @@ package org.ll.ai_recommendation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AiRecommendationApplication {
 
     public static void main(String[] args) {
